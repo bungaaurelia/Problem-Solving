89cdc016430b1ab3b0d1c929d19dabc9ef9f4dc2
bungaaurelia <annisabungaaurelia99@gmail.com>
2021-10-11 14:46:20 +0700

    Problem7

---
+++ Assets/Scrpts/DestroySquare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroySquare : MonoBehaviour
+{
+    public Text scoreText;
+    int score;
+
+    private void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("square"))
+        {
+            Destroy(collision.gameObject);
+            score++;
+        }
+    }
+}
