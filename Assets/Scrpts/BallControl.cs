cad1b1783bcd8d779e8af3db02814c34c008156e
bungaaurelia <annisabungaaurelia99@gmail.com>
2021-10-11 14:40:21 +0700

    Problem5

--- Assets/Scrpts/BallControl.cs
+++ Assets/Scrpts/BallControl.cs
@@ -4,63 +4,19 @@ using UnityEngine;
 
 public class BallControl : MonoBehaviour
 {
-    // Rigidbody 2D bola
-    private Rigidbody2D rigidBody2D;
+    public float maxMoveSpeed = 10;
+    public float smoothTime = 0.3f;
+    public float minDistance = 2;
 
-    // Besarnya gaya awal yang diberikan untuk mendorong bola
-    public float xInitialForce;
-    public float yInitialForce;
+    Vector2 currentVelocity;
 
-    void ResetBall()
-    {
-        // Reset posisi menjadi (0,0)
-        transform.position = Vector2.zero;
-
-        // Reset kecepatan menjadi (0,0)
-        rigidBody2D.velocity = Vector2.zero;
-    }
-
-    void PushBall()
-    {
-        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
-        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
-
-        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
-        float randomDirection = Random.Range(0, 2);
-
-        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
-        // Jika tidak, bola bergerak ke kanan.
-        if (randomDirection < 1.0f)
-        {
-            // Gunakan gaya untuk menggerakkan bola ini.
-            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
-        }
-        else
-        {
-            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
-        }
-    }
-
-    void RestartGame()
-    {
-        // Kembalikan bola ke posisi semula
-        ResetBall();
-
-        // Setelah 2 detik, berikan gaya ke bola
-        Invoke("PushBall", 2);
-    }
-
-    void Start()
-    {
-        rigidBody2D = GetComponent<Rigidbody2D>();
-
-        // Mulai game
-        RestartGame();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Offsets the target position so that the object keeps its distance.
+        mousePosition += ((Vector2)transform.position - mousePosition).normalized * minDistance;
+
+        transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
     }
 }
