c8be6e98513616d69388b7acfc703bf3d7cfca8a
bungaaurelia <annisabungaaurelia99@gmail.com>
2021-10-11 14:58:07 +0700

    Problem9

---
+++ Bubble Pop/Assets/Scripts/Score.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public GameObject CanvasWin;
+    public Text scoreText;
+    int score;
+    public float Maxscore = 10f;
+    public GameObject Arrow;
+    public GameObject MusicBackground;
+    public bool GameAktif = true;
+    public float Waktu;
+    
+    float s;
+
+    void SetText()
+    {
+        int Menit = Mathf.FloorToInt(Waktu / 60);
+        int Detik = Mathf.FloorToInt(Waktu % 60);
+    }
+    private void Update()
+    {
+        if (GameAktif)
+        {
+            s += Time.deltaTime;
+            if (s >= 1)
+            {
+                Waktu--;
+                s = 0;
+            }
+        }
+
+
+        scoreText.text = score.ToString();
+
+        if (score == 50 )
+        {
+            Debug.Log("You WIN!");
+            CanvasWin.SetActive(true);
+            Arrow.SetActive(false);
+            MusicBackground.SetActive(false);
+            PlaySound.Instance.PlaySFX("win");
+            GameAktif = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("bubble"))
+        {
+            Destroy(collision.gameObject);
+            score++;
+            PlaySound.Instance.PlaySFX("bubble");
+        }
+    }
+
+
+}
---
+++ Bubble Pop/Assets/Scripts/TimerSetting.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerSetting : MonoBehaviour
+{
+    public Text TextTimer;
+    public float Waktu;
+    public bool GameAktif = true;
+    public GameObject CanvasGO;
+    public GameObject Arrow;
+    public GameObject MusicBackground;
+
+    void SetText()
+    {
+        int Menit = Mathf.FloorToInt(Waktu / 60);
+        int Detik = Mathf.FloorToInt(Waktu % 60);
+        TextTimer.text = Menit.ToString("00") +":"+ Detik.ToString("0");
+    }
+
+    float s;
+
+    private void Update()
+    {
+        if (GameAktif)
+        {
+            s += Time.deltaTime;
+            if (s >= 1)
+            {
+                Waktu--;
+                s = 0;
+            }
+        }
+
+
+
+        if(GameAktif && Waktu <=0)
+        {
+            Debug.Log("Game Over");
+            CanvasGO.SetActive(true);
+            GameAktif = false;
+            Arrow.SetActive(false);
+            MusicBackground.SetActive(false);
+            PlaySound.Instance.PlaySFX("gameover");
+        }
+
+        SetText();
+        
+    }
+}
